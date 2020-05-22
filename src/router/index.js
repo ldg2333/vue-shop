45ab9babd885f6a1b88ad09bd74e01b0c837093a
刘大港 <1782646017@qq.com>
2020-05-22 17:08:34 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
+import Home from '../components/home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 
